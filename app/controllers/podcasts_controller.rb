26f98e454090ed5fdd880d6bdd40af478c8a4afa
Alex Hanna <tinnvec@gmail.com>
2014-03-21 16:11:51 -0500

    Better Feed url validation handling

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -11,15 +11,17 @@ class PodcastsController < ApplicationController
   def create
     begin
       v = W3C::FeedValidator.new()
-      if v.validate_url(params[:podcast][:feed_url]) && v.valid?
+      if v.validate_url(params[:podcast][:feed_url])
         if current_user.podcasts << Podcast.find_or_create_by(feed_url: params[:podcast][:feed_url])
           flash[:success] = "Successfully Subscribed."
         else
           flash[:danger] = "There was an Error."
         end
+      else
+        flash[:danger] = "Invalid Url for Feed."
       end
     rescue
-      flash[:danger] = "Invalid Url for Feed."
+      flash[:danger] = "Exception caught when trying to add Feed."
     end
     redirect_to current_user
   end
