c2d0e3c4f5c2f917134ad51e684a7e7dfdbd9f69
Alex Hanna <tinnvec@gmail.com>
2014-03-22 11:15:21 -0500

    Seperate testing of feed url out in to it's own before_filter method

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -3,6 +3,7 @@ class PodcastsController < ApplicationController
   require 'feed_validator'
 
   before_filter :authenticate_user!, only: [ :create, :destroy ]
+  before_filter :validate_podcast, only: [ :create ]
 
   def index
     @podcasts = Podcast.all
@@ -13,19 +14,10 @@ class PodcastsController < ApplicationController
   end
 
   def create
-    begin
-      v = W3C::FeedValidator.new()
-      if v.validate_url(params[:podcast][:feed_url])
-        if current_user.podcasts << Podcast.find_or_create_by(feed_url: params[:podcast][:feed_url])
-          flash[:success] = "Successfully Subscribed."
-        else
-          flash[:danger] = "There was an Error."
-        end
-      else
-        flash[:danger] = "Invalid Url for Feed."
-      end
-    rescue
-      flash[:danger] = "Exception caught when trying to add Feed."
+    if current_user.podcasts << Podcast.find_or_create_by(feed_url: params[:podcast][:feed_url])
+      flash[:success] = "Successfully Subscribed."
+    else
+      flash[:danger] = "There was an Error."
     end
     redirect_to current_user
   end
@@ -39,5 +31,24 @@ class PodcastsController < ApplicationController
 
     def podcast_params
       params.require(:podcast).permit(:feed_url)
+    end
+
+    def validate_podcast
+      begin
+        v = W3C::FeedValidator.new()
+        if v.validate_url(params[:podcast][:feed_url])
+          feed = Feedjira::Feed.fetch_and_parse(params[:podcast][:feed_url])
+          if !defined? feed.title
+            flash[:danger] = "Feed has no title, probably not valid."
+            redirect_to current_user
+          end
+        else
+          flash[:danger] = "Invalid Url."
+          redirect_to current_user
+        end
+      rescue
+        flash[:danger] = "Exception caught when trying to add Podcast."
+        redirect_to current_user
+      end
     end
 end
