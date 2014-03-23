322a2ac6517f897ef2d695c7377f8561c5d8acd0
Alex Hanna <tinnvec@gmail.com>
2014-03-23 14:30:11 -0500

    Add missing check for signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,16 @@ class HomeController < ApplicationController
   include PodcastsHelper
 
   def index
-    entry_list = []
-    current_user.podcasts.each do |podcast|
-      get_feed(podcast).entries.each do |entry|
-        entry_list << entry
+    if user_signed_in?
+      entry_list = []
+      current_user.podcasts.each do |podcast|
+        get_feed(podcast).entries.each do |entry|
+          entry_list << entry
+        end
       end
+      entry_list = entry_list.sort_by { |entry| entry.published }
+      @entry_list_by_day = entry_list.group_by { |entry| entry.published.strftime("%B #{entry.published.day.ordinalize}, %Y") }
+      @entry_list_by_day = @entry_list_by_day.sort_by { |date, entry_list| [Date.parse(date).month, Date.parse(date).day] }.reverse
     end
-    entry_list = entry_list.sort_by { |entry| entry.published }
-    @entry_list_by_day = entry_list.group_by { |entry| entry.published.strftime("%B #{entry.published.day.ordinalize}, %Y") }
-    @entry_list_by_day = @entry_list_by_day.sort_by { |date, entry_list| [Date.parse(date).month, Date.parse(date).day] }.reverse
   end
 end
