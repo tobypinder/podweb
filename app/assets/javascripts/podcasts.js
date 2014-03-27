df7fc183c7025b557023e2d024ced63098cfc78b
Alex Hanna <tinnvec@gmail.com>
2014-03-27 18:50:57 -0500

    ACTUALLY make less calls now when remembering timecodes, had to implement a timing check to make sure we're not submitting more than 1 db call every update

--- app/assets/javascripts/podcasts.js
+++ app/assets/javascripts/podcasts.js
@@ -1,11 +1,14 @@
 (function($) {
   $(document).ready(function() {
+    last_update = new Date();
     $('audio, video').on('timeupdate', function(event) {
       currentTime = parseInt($(this)[0].currentTime);
       epid = $(this).data('epid');
+      now = new Date();
       $('#timecode' + epid).val(currentTime);
-      if ( (currentTime % 10 == 0) && (currentTime != 0) ) {
-        setTimeout(function(){ $('#new_watched_episode' + epid).submit(); }, 1000);
+      if ( (currentTime != 0) && (currentTime % 10 == 0) && (now - last_update > 1000)) {
+        last_update = new Date();
+        $('#new_watched_episode' + epid).submit();
       }
     });
   });
