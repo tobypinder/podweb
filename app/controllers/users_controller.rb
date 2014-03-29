83afcb04f87f3ef5ff63df3a5507f8b698276ad1
Alex Hanna <tinnvec@gmail.com>
2014-03-28 19:16:39 -0500

    Clean up users show view a bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @podcasts = @user.podcasts.sort_by { |podcast| podcast.title.sub(/^(the|a|an)\s+/i, '') }
   end
 
 end
