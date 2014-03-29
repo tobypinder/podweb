54ed9d26335c2547263410bd818730a2a0e911bc
Alex Hanna <tinnvec@gmail.com>
2014-03-28 19:28:34 -0500

    Paginate podcasts on users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @podcasts = @user.podcasts.sort_by { |podcast| podcast.title.sub(/^(the|a|an)\s+/i, '') }
+    @podcasts = @user.podcasts.sort_by { |podcast| podcast.title.sub(/^(the|a|an)\s+/i, '') }.paginate(page: params[:page], per_page: 12)
   end
 
 end
