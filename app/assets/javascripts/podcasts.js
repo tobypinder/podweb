f31994dbc562f5c35180239e7a91d6d37ad6c777
Alex Hanna <tinnvec@gmail.com>
2014-04-02 12:07:48 -0500

    Rearrange and comment podcasts.js

--- app/assets/javascripts/podcasts.js
+++ app/assets/javascripts/podcasts.js
@@ -5,50 +5,65 @@
     var currentTime;
     var endTime;
 
-    if ('#playlist-player') {
-      setPlayerEpisode($('#media-playlist tr').first());
-      $('#media-playlist tr').click(function(event) {
-        event.preventDefault();
-        $('tr[data-epid="' + current_epid + '"]').removeClass('active');
-        $('tr[data-epid="' + current_epid + '"] span.label').removeClass('label-primary label-warning label-default');
-        if (currentTime < 1) {
-          $('tr[data-epid="' + current_epid + '"]').addClass('info');
-          $('tr[data-epid="' + current_epid + '"] span.label').addClass('label-primary').html('Unwatched');
-        } else if (endTime - currentTime > 15) {
-          $('tr[data-epid="' + current_epid + '"]').addClass('warning');
-          $('tr[data-epid="' + current_epid + '"] span.label').addClass('label-warning').html('Watched Some');
-        } else {
-          $('tr[data-epid="' + current_epid + '"] span.label').addClass('label-default').html('Watched');
-        }
-        setPlayerEpisode($(this));
-      });
-    }
+    // When clicking a playlist item
+    $('#media-playlist tr').click(function(event) {
+      event.preventDefault();
 
-    $('.podcast').each(function(index, podcast) {
-      if ((index + 1) % 2 == 0) { $(podcast).after('<div class="clearfix visible-xs"></div>') };
-      if ((index + 1) % 3 == 0) { $(podcast).after('<div class="clearfix visible-sm"></div>') };
-      if ((index + 1) % 4 == 0) { $(podcast).after('<div class="clearfix visible-md"></div>') };
-      if ((index + 1) % 6 == 0) { $(podcast).after('<div class="clearfix visible-lg"></div>') };
+      // Switch CSS classes of playlist item we just switched FROM
+      $('tr[data-epid="' + current_epid + '"]').removeClass('active');
+      $('tr[data-epid="' + current_epid + '"] span.label').removeClass('label-primary label-warning label-default');
+      if (currentTime < 1) {
+        $('tr[data-epid="' + current_epid + '"]').addClass('info');
+        $('tr[data-epid="' + current_epid + '"] span.label').addClass('label-primary').html('Unwatched');
+      } else if (endTime - currentTime > 15) {
+        $('tr[data-epid="' + current_epid + '"]').addClass('warning');
+        $('tr[data-epid="' + current_epid + '"] span.label').addClass('label-warning').html('Watched Some');
+      } else {
+        $('tr[data-epid="' + current_epid + '"] span.label').addClass('label-default').html('Watched');
+      }
+
+      // Switch CSS classes of playlist item we just switched TO
+      $(this).removeClass('active warning info');
+      $(this).addClass('active');
+
+      // Load episode info into various display elements
+      var epid = $(this).attr('data-epid');
+      $('#episode_summary').attr('data-target', '#full-entry-episode' + epid);
+      $('#episode_summary div.well').html($(this).attr('data-shortsummary'));
+      $('#episode_description div.modal').attr({ id: 'full-entry-episode' + epid, 'aria-labelledby': 'modal-label-episode' + epid });
+      $('#episode_description h4.modal-title').attr('id', 'modal-label-episode' + epid);
+      $('#episode_description h4.modal-title').html($(this).children('.episode-title').html());
+      $('#episode_description div.modal-body').html($(this).attr('data-longsummary'));
+      $('form').attr('id', 'new_watched_episode' + epid);
+      $('form input:nth-last-child(2)').val(epid);
+      $('form input:nth-last-child(1)').attr('id', 'timecode' + epid);
+      $('#playlist-player').attr('data-epid', epid);
+      $('#playlist-player').attr('src', $(this).attr('data-mediaurl'));
+      $('#playlist-player').load();
     });
 
+    // When starting to load a new media file, reset current time and episode id
     $('audio, video').on('loadstart', function(event) {
       currentTime = 0;
       current_epid = $(this).attr('data-epid');
     });
 
+    // When we have updated info for end and current times, update that info
     $('audio, video').on('durationchange', function(event) {
       endTime = parseInt($(this)[0].duration);
       currentTime = parseInt($(this)[0].currentTime);
     });
 
+    // When we've loaded the media file, make sure current time is correct
     $('audio, video').on('loadeddata', function(event) {
       currentTime = parseInt($(this)[0].currentTime);
     });
 
+    // Trigger update and possibly storage of episode time information when player updates time
     $('audio, video').on('timeupdate', function(event) {
       var now = new Date();
       currentTime = parseInt($(this)[0].currentTime);
-      
+
       if (endTime - currentTime < 15) {
         $('input#watched_episode_watched').val("true");
       } else {
@@ -56,37 +71,26 @@
       }
       $('input#timecode' + current_epid).val(currentTime);
 
+      // Store in database every 10 seconds, at the end of media file, or when media file is paused.
+      // Also make sure we're not interacting wiht the database more than once per second
       if ( ((currentTime != 0) && (currentTime % 10 == 0) && (now - last_update > 1000)) || ($(this)[0].ended || $(this)[0].paused) ) {
         last_update = new Date();
         $('tr[data-epid="' + current_epid + '"').attr('data-mediaurl', $(this).attr('src').replace(/#t=.*/, '') + '#t=' + currentTime );
         $('#new_watched_episode' + current_epid).submit();
       }
     });
-  });
 
-  function setPlayerEpisode(episodeLink) {
-    var epid = episodeLink.attr('data-epid');
+    // Load player with first video in playlist
+    if ('#playlist-player') {
+      $('#media-playlist tr').first().trigger('click');
+    }
 
-    episodeLink.removeClass('warning info');
-    episodeLink.addClass('active');
-
-    $('#episode_summary').attr('data-target', '#full-entry-episode' + epid);
-    $('#episode_summary div.well').html(episodeLink.attr('data-shortsummary'));
-
-    $('#episode_description div.modal').attr({
-      id: 'full-entry-episode' + epid,
-      'aria-labelledby': 'modal-label-episode' + epid
+    // For multiple podcast displays (library, subscriptions) insert breaks depending on device size
+    $('.podcast').each(function(index, podcast) {
+      if ((index + 1) % 2 == 0) { $(podcast).after('<div class="clearfix visible-xs"></div>') };
+      if ((index + 1) % 3 == 0) { $(podcast).after('<div class="clearfix visible-sm"></div>') };
+      if ((index + 1) % 4 == 0) { $(podcast).after('<div class="clearfix visible-md"></div>') };
+      if ((index + 1) % 6 == 0) { $(podcast).after('<div class="clearfix visible-lg"></div>') };
     });
-    $('#episode_description h4.modal-title').attr('id', 'modal-label-episode' + epid);
-    $('#episode_description h4.modal-title').html(episodeLink.children('.episode-title').html());
-    $('#episode_description div.modal-body').html(episodeLink.attr('data-longsummary'));
-
-    $('form').attr('id', 'new_watched_episode' + epid);
-    $('form input:nth-last-child(2)').val(epid);
-    $('form input:nth-last-child(1)').attr('id', 'timecode' + epid);
-
-    $('#playlist-player').attr('data-epid', epid);
-    $('#playlist-player').attr('src', episodeLink.attr('data-mediaurl'));
-    $('#playlist-player').load();
-  }
+  });
 })(jQuery);
